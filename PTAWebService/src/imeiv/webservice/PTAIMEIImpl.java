bc1c11b233f029edfc56e3e5c7843c18440dc240
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2022-02-27 16:39:42 +0500

    adding verification for imei

--- PTAWebService/src/imeiv/webservice/PTAIMEIImpl.java
+++ PTAWebService/src/imeiv/webservice/PTAIMEIImpl.java
@@ -9,6 +9,9 @@ import javax.ejb.Stateless;
 import imeiv.ptamodel.PTASessionEJBLocal;
 
 import imeiv.ptamodel.VwItemImeiInformation;
+import imeiv.ptamodel.PTARequestResult;
+
+import java.util.ArrayList;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -33,11 +36,41 @@ public class PTAIMEIImpl {
     @GET
     @Produces("application/json")
     @Path("/")
-    public List<VwItemImeiInformation> getVwItemImeiInformationFindByIMEI(@QueryParam("pimei1") String pimei1,
+    public List<PTARequestResult> getVwItemImeiInformationFindByIMEI(@QueryParam("pimei1") String pimei1,
                                                                           @QueryParam("pimei2") String pimei2,
                                                                           @QueryParam("papikey") String papikey) {
-
-        return mySessionBean.getVwItemImeiInformationFindByIMEI(pimei1, pimei2, papikey);
+        List<VwItemImeiInformation> limeiinfo=mySessionBean.getVwItemImeiInformationFindByIMEI(pimei1, pimei2, papikey);
+        
+        PTARequestResult ptares=null;
+        if (limeiinfo.isEmpty()) {
+           if (papikey==null || !papikey.equals("D$pL0y@cC$sSk$y")) {///for invalid key
+               ptares=new PTARequestResult(0,401,"Unauthorized Request, api key is missing/invalid");         
+           }
+           else
+           if (pimei1==null || pimei1.length()<=13 ||pimei2==null || pimei2.length()<=13)  {///for invalid key
+               ptares=new PTARequestResult(0,406,"Invalid or empty IMEI");         
+           } 
+           
+           else
+           {
+               ptares=new PTARequestResult(0,404,"Resource Not found");         
+           
+           }
+       }
+        else {
+            ptares=new PTARequestResult(1,200,"Operation Successful.");    
+        }
+//        VwItemImeiInformation info=limeiinfo.get(0);
+        
+        List<PTARequestResult> item=new ArrayList<PTARequestResult>();
+        item.add(ptares);
+        
+        return item;
+//        System.out.println(info.getStatus());
+//        System.out.println(info.getImei1());
+//        System.out.println(info.getImei2());
+//        
+//        return limeiinfo;
 
     }
 
