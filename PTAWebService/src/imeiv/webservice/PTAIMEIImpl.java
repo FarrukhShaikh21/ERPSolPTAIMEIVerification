2da37dd2ad94fb51418d10c088ca89272a3c59ce
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2022-02-27 15:38:32 +0500

    api completed

--- PTAWebService/src/imeiv/webservice/PTAIMEIImpl.java
+++ PTAWebService/src/imeiv/webservice/PTAIMEIImpl.java
@@ -10,8 +10,16 @@ import imeiv.ptamodel.PTASessionEJBLocal;
 
 import imeiv.ptamodel.VwItemImeiInformation;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+
 
 @Stateless
+@Path("webservice")
+@Consumes("application/json")
 
 
 public class PTAIMEIImpl {
@@ -22,8 +30,12 @@ public class PTAIMEIImpl {
     PTASessionEJBLocal mySessionBean;
 
 
-    public List<VwItemImeiInformation> getVwItemImeiInformationFindByIMEI(String pimei1, String pimei2,
-                                                                          String papikey) {
+    @GET
+    @Produces("application/json")
+    @Path("/")
+    public List<VwItemImeiInformation> getVwItemImeiInformationFindByIMEI(@QueryParam("pimei1") String pimei1,
+                                                                          @QueryParam("pimei2") String pimei2,
+                                                                          @QueryParam("papikey") String papikey) {
 
         return mySessionBean.getVwItemImeiInformationFindByIMEI(pimei1, pimei2, papikey);
 
